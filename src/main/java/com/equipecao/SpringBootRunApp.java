71ef9ef0381e59aef8f55ec2aa6ed00a15cd4b15
mrapolli <miguel.rapolli@gmail.com>
2019-05-03 17:09:08 -0300

    refactor

--- src/main/java/com/equipecao/SpringBootRunApp.java
+++ src/main/java/com/equipecao/SpringBootRunApp.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SpringBootDroolsHelloWorldApp {
+public class SpringBootRunApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDroolsHelloWorldApp.class, args);
+		SpringApplication.run(SpringBootRunApp.class, args);
 
 	}
 
